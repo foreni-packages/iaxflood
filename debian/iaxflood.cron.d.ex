9c773e75bb49c7609ae233d4a742c7e3007065fd
Devon Kearns <dookie@kali.org>
2012-12-09 09:28:50 -0700

    pre-build commit

--- debian/iaxflood.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the iaxflood package
-#
-0 4	* * *	root	[ -x /usr/bin/iaxflood_maintenance ] && /usr/bin/iaxflood_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(iaxflood):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="iaxflood" command="/usr/bin/iaxflood"
